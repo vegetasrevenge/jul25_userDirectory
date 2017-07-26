989d0886b52d30f47d516b863f3d3148ad643330
vegetasrevenge <cjedwards233@yahoo.com>
2017-07-25 22:10:29 -0500

    check out those transitions

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const mustache = require('mustache-express');
-
 const app = express();
+const path = require('path');
+const data = require('./data');
 
 app.engine('mustache', mustache());
-app.set('view engine', mustache);
+app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res){
+  res.render('index', data);
+});
+
+// app.get('/data', function(req, res) {
+//   res.json(data);
+// });
+
+
 
 app.listen(5687, function(){
-  console.log('Here we go!');
-})
+  console.log('Here we go, port 5687!');
+});
