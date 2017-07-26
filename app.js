4cbd5438bf60e610b4478c8be919ee98e7ef73d9
vegetasrevenge <cjedwards233@yahoo.com>
2017-07-26 16:16:28 -0500

    adjusted layout issues

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const mustache = require('mustache-express');
 const app = express();
-const path = require('path');
 const data = require('./data');
 
-app.engine('mustache', mustache());
+app.use(express.static('public'));
+
+//Doug clued this in: http://www.douglashirsh.com/2017/07/26/node-express-mustache-template-caching/
+let mustacheInstance = mustache();
+mustacheInstance.cache = null;
+app.engine('mustache', mustacheInstance);
+
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
+//instead, use -->
 app.get('/', function(req, res){
   res.render('index', data);
 });
 
-// app.get('/data', function(req, res) {
-//   res.json(data);
-// });
-
-
+app.get('/details/:id', function(req, res) {
+  let profile =
+    data.users.find(function(item){
+      return item.id == req.params.id;
+    });
+    res.render('details', profile);
+    console.log(profile);
+});
 
 app.listen(5687, function(){
   console.log('Here we go, port 5687!');
